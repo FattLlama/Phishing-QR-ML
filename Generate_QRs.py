d4a2830fd44cf3e25c24f66f232a7009a1c78232
FattLlama <108482247+FattLlama@users.noreply.github.com>
2024-04-07 10:13:38 -0400

    Trained-on-whole-set
    Saved a model trained 2 epochs on the whole dataset

--- Generate_QRs.py
+++ Generate_QRs.py
@@ -26,13 +26,27 @@ def create_directories(good_dir, bad_dir):
     if not os.path.exists(bad_dir):
         os.makedirs(bad_dir)
 
+def get_small_set(data):
+    #2.5 to 1 ratio
+    good_chunk = data[data['Label'] == 'good']
+    bad_chunk = data[data['Label'] == 'bad']
+    both = pd.concat([good_chunk.iloc[:35000], bad_chunk.iloc[:15000]], ignore_index=True)
+    output = both.sample(frac=1).reset_index(drop=True)
+    return output
+
+def get_micro_set(data):
+    micro_data = data[data['URL'].str.len() < 26]
+    micro_data = micro_data.reset_index()
+    return micro_data
+
 # Read CSV file
 csv_file = './phishing_site_urls.csv' # Change this to your CSV file name
 df = pd.read_csv(csv_file)
 # Output directories
-good_dir = './QR_codes/good_qr_codes'
-bad_dir = './QR_codes/bad_qr_codes'
+good_dir = './micro_QR_codes/good_qr_codes'
+bad_dir = './micro_QR_codes/bad_qr_codes'
 create_directories(good_dir, bad_dir)
+df = get_micro_set(df)
 
 # Generate QR codes
 for index, row in df.iterrows():
